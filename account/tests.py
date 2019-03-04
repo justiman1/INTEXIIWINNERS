e63623eed2a958cd4bcb1253f17ab4f9f76d1d18
Justin <jschwendy95@gmail.com>
2019-03-04 07:53:15 -0700

    stuff

--- account/tests.py
+++ account/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from account import models as amod
+from django.contrib.auth import models as pmod
 from datetime import datetime
 from lxml import etree
+from django.contrib.auth.management.commands import changepassword
+from django.contrib.auth import models, management
+# from StringIO import StringIO
 
 class AccountTests(TestCase):
     fixtures = [ 'account.yaml' ]
@@ -34,33 +38,21 @@ class AccountTests(TestCase):
         response = self.client.post('/account/login/', credentials)
         # get the request object (testing framework embeds it as response.wsgi_request)
         request = response.wsgi_request
-        # this next line is ONLY for debugging the test - it should be removed after things work
-        self.print_html(response.content)
         # if it worked, then request.user will be the homer object and is_authenticated will be true
         self.assertTrue(request.user.is_authenticated, msg="User should have authenticated")
         self.assertEqual(request.user.id, self.homer.id, msg="User should have been homer")
         # if it worked, the response should be a redirect code (login.py returned HttpResponseRedirect)
         self.assertEqual(response.status_code, 302, msg="User wasn't redirected")
 
-    def print_html(self, content):
-        '''Helper to pretty-print HTML'''
-        content = content.strip()
-        if content:
-            html = etree.HTML(content)
-            print(etree.tostring(html, pretty_print=True, encoding=str))
-        else:
-            print('<empty content>')
-
-    # @classmethod
-    # def setUpTestData(cls):
-    #     # Set up data for the whole TestCase
-    #     cls.foo = Foo.objects.create(bar="Test")
-    #     ...
+    def test_user_logout(self):
+        response = self.client.post('/account/logout')
+        request = response.wsgi_request
+        self.assertTrue(request.user.is_anonymous, msg="User should not be authenticated")
 
     def test_user_get(self):
-        u1 = amod.User.objects.get(id=2)
-        self.assertEqual(u1.username, 'homer', msg="Username should have been homer")
-        self.assertEqual(u1.check_password(''), msg="Wrong password")
+        u1 = amod.User.objects.get(id=3)
+        self.assertEqual(u1.username, 'user1', msg="Username should have been user0")
+        self.assertEqual(u1.password, u1.check_password('asdf'), msg="Wrong password")
 
     def test_user_create(self):
         u1 = amod.User()
@@ -69,3 +61,23 @@ class AccountTests(TestCase):
         u2 = amod.User.objects.get(id=u1.id)
 
         self.assertEqual(u1.username, u2.username, msg="Username didn't match")
+
+
+    def test_change_user_password(self):
+        u1 = amod.User.objects.get(id=3)
+
+    def test_group_permission(self):
+        p = pmod.Permission()
+        p.codename = 'NewPermission'
+        p.name = 'New Permission'
+        p.content_type = pmod.Permission.objects.get(codename = 'add_user').content_type
+        p.save()
+        g1 = pmod.Group()
+        g1.name = 'NewGroup'
+        g1.save()
+        g1 = pmod.Group.objects.get(name='NewGroup')
+        g1.permissions.add(pmod.Permission.objects.get(codename='NewPermission'))
+        u1 = amod.User.objects.get(id=3)
+        g1 = pmod.Group.objects.get(name='NewGroup')
+        u1.groups.add(g1)
+        self.assertTrue(u1.has_perm('account.NewPermission'))
