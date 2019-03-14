fa2bf1d4b0d663af63427378f0b0c474893db009
Justin <jschwendy95@gmail.com>
2019-03-13 21:40:31 -0600

    Fixed

---
+++ catalog/templates/__dmpcache__/product.tile.html.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1552534546.4166665
+_enable_loop = True
+_template_filename = 'C:/Users/justi/OneDrive/Desktop/School/Winter 2019/IS 413/sprint/catalog/templates/product.tile.html'
+_template_uri = 'product.tile.html'
+_source_encoding = 'utf-8'
+import django_mako_plus
+import django.utils.html
+_exports = []
+
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        __M_writer = context.writer()
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "C:/Users/justi/OneDrive/Desktop/School/Winter 2019/IS 413/sprint/catalog/templates/product.tile.html", "uri": "product.tile.html", "source_encoding": "utf-8", "line_map": {"18": 0, "28": 18}}
+__M_END_METADATA
+"""
